091d1ddd88a03a5fc3a4795e480bfe91d9d2527e
Arju Paul <arjupaul2006@gmail.com>
2026-08-14 01:04:55 +0530

    Hyperparameter Completed

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import sys
 # pyrefly: ignore [missing-import]
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -17,12 +18,19 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
 
-def evalute_model(X_train, y_train, X_test, y_test, models):
+def evalute_model(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
         for i in range(len(list(models))):
             model = list(models.values())[i]
-    
+            param = list(params.values())[i]
+
+            # determine the best hyperparameter
+            gs = GridSearchCV(estimator=model, param_grid=param, cv=3)
+            gs.fit(X_train, y_train)
+
+            # set the best hyperparameter
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
     
             y_train_pred = model.predict(X_train)
